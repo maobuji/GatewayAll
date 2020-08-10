5d05b0054ebe5b675f50ec6ed7692517070b2429
maobuji <16770864@qq.com>
2020-08-10 11:11:50 +0800

    修改网络配置和帮助

--- gatewayClear.sh
+++ gatewayClear.sh
@@ -2,9 +2,16 @@
 docker-compose -p docker-apisix down
 rm apisix_log/* -rf
 rm etcd_conf/* -rf
+rm mysql_data/* -rf
+
+
+
 docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/apisix:latest
 docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/apisix-dashboard:latest
 docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/apisix-manager:latest
+docker rmi mysql:latest
+docker rmi grafana/grafana:latest
+docker rmi prom/prometheus:latest
 docker rmi bitnami/etcd:3.4.9
 docker rmi ruby:2-alpine
 docker images
