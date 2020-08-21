4ddcc53f6fcfca16e1674e1695d7ce009ea537a5
maobuji <16770864@qq.com>
2020-08-21 10:55:05 +0800

    添加镜像清理指令

--- gatewayClearDocker.sh
+++ gatewayClearDocker.sh
@@ -3,6 +3,10 @@
 docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/apisix:latest
 docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/apisix-dashboard:latest
 docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/apisix-manager:latest
+docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/logstash:latest
+docker rmi wurstmeister/kafka:2.12-2.2.1
+docker rmi zookeeper:3.5.5
+docker rmi sheepkiller/kafka-manager:latest
 docker rmi mysql:latest
 docker rmi grafana/grafana:latest
 docker rmi prom/prometheus:latest
